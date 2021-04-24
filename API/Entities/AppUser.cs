82f568d663b0e38cc958660128bc911cb1419416
Hussain <Hussainlala58@gmail.com>
2021-04-24 05:45:19 +0530

    Added Authentication For Users using JWT

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace API.Entities
     {   
         public int Id { get; set; } // Keep it Id or ID as it is convention and the program will auto increament it in the database
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
